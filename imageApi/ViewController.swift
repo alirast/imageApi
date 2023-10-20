79c57e4a6693650a9afba92400d5917e6af0be34
alirast <xdabnormal@gmail.com>
2023-10-20 12:52:40 +0300

    success (got json with link for good image without background)

--- imageApi/ViewController.swift
+++ imageApi/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-//7 34
 
 class ViewController: UIViewController {
     
@@ -17,7 +16,7 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named: "people")
         return imageView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
@@ -25,7 +24,7 @@ class ViewController: UIViewController {
         
         postAPI()
     }
-
+    
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -36,153 +35,63 @@ class ViewController: UIViewController {
     }
     
     func postAPI() {
-       
-        guard let url = URL(string: "https://universal-background-removal.p.rapidapi.com/cutout/universal/common-image") else {
-            print("url is wrong")
-            return
-        }
-        
-        var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
         let headers = [
             "content-type": "multipart/form-data; boundary=---011000010111000001101001",
-            "X-RapidAPI-Key": "b5bf1d4d9bmsh97142a47ad9c23bp11d9b4jsn591d0d82708f",
+            "X-RapidAPI-Key": "b17fb37ff3msh2a4c5dfe99530d0p1c86a6jsndfc55e574713",
             "X-RapidAPI-Host": "universal-background-removal.p.rapidapi.com"
         ]
         
-        request.allHTTPHeaderFields = headers
-        request.httpMethod = "POST"
-        
-        //[[String : String]] array of dictionaries
-        let requestObject = [
-            [
-                "name": "image",
-                "fileName": "people",
-                "contentType": "application/octet-stream",
-                "file": "[object File]"
-            ]
-        ]
-        do {
-            let requestBody = try JSONSerialization.data(withJSONObject: requestObject, options: .fragmentsAllowed)
-            request.httpBody = requestBody
-        } catch {
-            debugPrint("request error")
+        guard let imageData = imageView.image?.pngData() else {
+            print("Unable to convert image to data")
+            return
         }
         
+        let parameters = [
+            [
+                "name": "image",
+                "fileName": "people.jpeg",
+                "contentType": "application/octet-stream",
+                "file": imageData
+            ]
+        ]
+        
+        let boundary = "---011000010111000001101001"
+        
+        var body = Data()
+        
+        for param in parameters {
+            let paramName = param["name"]!
+            
+            body.append("--\(boundary)\r\n".data(using: .utf8)!)
+            body.append("Content-Disposition:form-data; name=\"\(paramName)\"; filename=\"\(param["fileName"]!)\"\r\n".data(using: .utf8)!)
+            body.append("Content-Type: \(param["contentType"]!)\r\n\r\n".data(using: .utf8)!)
+            body.append(param["file"] as! Data)
+            body.append("\r\n".data(using: .utf8)!)
+        }
+        
+        body.append("--\(boundary)--\r\n".data(using: .utf8)!)
+        
+        guard let url = URL(string: "https://universal-background-removal.p.rapidapi.com/cutout/universal/common-image") else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = body
+        
         let session = URLSession.shared
-        let dataTask = session.dataTask(with: request) { data, response, error in
-            //json parsing
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
             do {
-                guard let data = data else { return }
-                let parsingData = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                print(parsingData)
+                let json = try JSONSerialization.jsonObject(with: data)
+                print("JSON", json)
             } catch {
-                debugPrint("parsing with error")
+                print("ERROR", error)
             }
         }
         
         dataTask.resume()
     }
-    
-    /* 401
-     func postAPI() {
-        guard let url = URL(string: "https://universal-background-removal.p.rapidapi.com/cutout/universal/common-image") else { return }
-        let headers = [
-            "content-type": "multipart/form-data; boundary=---011000010111000001101001",
-            "X-RapidAPI-Key": "0431c3340cmshafab1ef65145605p1f0ebbjsneba9be3ede79",
-            "X-RapidAPI-Host": "universal-background-removal.p.rapidapi.com"
-        ]
-        
-        let parameters = [
-            [
-                "name": "image",
-                "fileName": "people",
-                "contentType": "application/octet-stream",
-                "file": "[object File]"
-            ]
-        ]
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters) else { return }
-        request.httpBody = httpBody
-        request.addValue("multipart/form-data; boundary=---011000010111000001101001", forHTTPHeaderField: "content-type")
-        
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print("response", response)
-            }
-            
-            guard let data = data else { return }
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: [])
-                print("json", json)
-            } catch {
-                print(error)
-            }
-        }.resume()
-    }*/
 }
-
-/*
- anyway 422
- func postApi() {
-     let headers = [
-         "content-type": "multipart/form-data; boundary=---011000010111000001101001",
-         "X-RapidAPI-Key": "fb79120e60msh1950408d6631cb0p12fac3jsn67e5f4bcb7a0",
-         "X-RapidAPI-Host": "universal-background-removal.p.rapidapi.com"
-     ]
-     
-     let parameters = [
-         [
-             "name": "image",
-             "fileName": "people.jpeg",
-             "contentType": "application/octet-stream",
-             "file": "[object File]"
-         ]
-     ]
-     
-     let boundary = "---011000010111000001101001"
-     
-     var body = ""
-     
-     for param in parameters {
-         let paramName = param["name"]!
-         
-         body += "--\(boundary)\r\n"
-         body += "Content-Disposition:form-data; name=\"\(paramName)\""
-         
-         if let filename = param["fileName"] {
-             let contentType = param["contentType"]!
-             
-             if let fileURL = Bundle.main.url(forResource: filename, withExtension: nil), let fileData = try? Data(contentsOf: fileURL) {
-                 body += "; filename=\"\(filename)\"\r\n"
-                 body += "Content-Type: \(contentType)\r\n\r\n"
-                 body += String(data: fileData, encoding: .utf8) ?? ""
-             }
-         }
-         
-         body += "\r\n\r\n"
-     }
-     
-     guard let url = URL(string: "https://universal-background-removal.p.rapidapi.com/cutout/universal/common-image") else {
-         return
-     }
-     
-     var request = URLRequest(url: url)
-     request.httpMethod = "POST"
-     request.allHTTPHeaderFields = headers
-     request.httpBody = body.data(using: .utf8)
-     
-     let session = URLSession.shared
-     
-     let dataTask = session.dataTask(with: request) { (data, response, error) in
-         if let error = error {
-             print(error)
-         } else if let httpResponse = response as? HTTPURLResponse {
-             print(httpResponse)
-         }
-     }
-     
-     dataTask.resume()
- }
- */
